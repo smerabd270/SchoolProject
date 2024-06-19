747773b21bebdaf87b7da6da677c3a1225459844
amer <a.abdalrazak@transtek.com>
2024-06-20 02:59:52 +0300

    t

--- SchoolProjectCore/ModuleICoreDependencies.cs
+++ SchoolProjectCore/ModuleICoreDependencies.cs
@@ -21,6 +21,7 @@ namespace SchoolProjectCore
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
           services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            //
             return services;
         }
     }
