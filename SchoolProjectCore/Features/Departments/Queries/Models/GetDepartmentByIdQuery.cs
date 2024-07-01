8821111c72221ea1d464366533ea8ce48eb4d0f5
Amer <a.abdalrazak@transtek.com>
2024-07-01 15:11:30 +0300

    t

---
+++ SchoolProjectCore/Features/Departments/Queries/Models/GetDepartmentByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SchoolProjectCore.Base;
+using SchoolProjectCore.Features.Departments.Queries.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProjectCore.Features.Departments.Queries.Models
+{
+    public class GetDepartmentByIdQuery:IRequest<Response<GetDepartmentByIdResponse>>
+    {
+        public int Id { get; set; }
+
+        public GetDepartmentByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ SchoolProjectCore/Features/Departments/Queries/Response/GetDepartmentByIdResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProjectCore.Features.Departments.Queries.Response
+{
+    public  class GetDepartmentByIdResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ManagerName { get; set; }
+        public class StudentResponse
+        {
+            public int Id { get; set; }
+            public string Name { get; set;}
+            public List<SubjectResponse>? SubjectList { get; set; }
+            public List<StudentResponse>? StudentList { get; set; }
+            public List<InstructortResponse>? InstructortList { get; set; }
+
+
+        }
+        public class SubjectResponse
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class InstructortResponse
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
