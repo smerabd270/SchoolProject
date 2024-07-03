7b74cc9dec0bb8e60628e6f453070cc6bef8d921
Amer <a.abdalrazak@transtek.com>
2024-07-03 16:56:07 +0300

    t

---
+++ SchoolProjectInfrastcure/Migrations/20240703135222_mmmmmmmmmmmmm.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProjectInfrastrcure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mmmmmmmmmmmmm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
