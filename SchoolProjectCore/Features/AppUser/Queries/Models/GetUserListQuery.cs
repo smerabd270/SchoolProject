a88a69febb6091e7e729a220a57c0ce2444bd12a
Amer <a.abdalrazak@transtek.com>
2024-07-09 16:44:24 +0300

    t

---
+++ SchoolProjectCore/Features/AppUser/Queries/Models/GetUserListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SchoolProjectCore.Features.AppUser.Queries.Response;
+using SchoolProjectCore.Warppers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProjectCore.Features.AppUser.Queries.Models
+{
+    public  class GetUserListQuery:IRequest<PaginatedResult<GetUserListResponse>>
+    {
+    }
+}
