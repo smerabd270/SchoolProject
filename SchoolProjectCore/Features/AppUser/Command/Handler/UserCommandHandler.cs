ac3d2c450aacb5d02c91ad972a68ca776dee799d
Amer <a.abdalrazak@transtek.com>
2024-07-14 14:16:11 +0300

    t

--- SchoolProjectCore/Features/AppUser/Command/Handler/UserCommandHandler.cs
+++ SchoolProjectCore/Features/AppUser/Command/Handler/UserCommandHandler.cs
@@ -15,17 +15,19 @@ using System.Threading.Tasks;
 
 namespace SchoolProjectCore.Features.AppUser.Command.Handler
 {
-    public class UserCommandHandler  : ResponseHandler,
-        IRequestHandler<AddUserCommand, Response<string>>
+    public class UserCommandHandler : ResponseHandler,
+        IRequestHandler<AddUserCommand, Response<string>>,
+                IRequestHandler<EditUserCommand, Response<string>>
+
     {
         private readonly IMapper _mapper;
         private readonly IStringLocalizer<SharedResources> _stringLocalizer;
-      private readonly UserManager<User> _userManager;
+        private readonly UserManager<User> _userManager;
         public UserCommandHandler(IMapper mapper, IStringLocalizer<SharedResources> stringLocalizer, UserManager<User> userManager) : base(stringLocalizer)
         {
             _mapper = mapper;
             _stringLocalizer = stringLocalizer;
-          _userManager = userManager;
+            _userManager = userManager;
         }
 
 
@@ -42,15 +44,38 @@ namespace SchoolProjectCore.Features.AppUser.Command.Handler
                 return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.BadRequest]);
 
             }
-    var newUser = _mapper.Map<User>(request);
-    var createUser = await _userManager.CreateAsync(newUser);
-            if (!createUser.Succeeded) 
+            var newUser = _mapper.Map<User>(request);
+            var createUser = await _userManager.CreateAsync(newUser);
+            if (!createUser.Succeeded)
             {
                 return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.BadRequest]);
 
             }
 
-return SuccessMessage<string>(_stringLocalizer[SharedResourcesKeys.Success]);
+            return SuccessMessage<string>(_stringLocalizer[SharedResourcesKeys.Success]);
+        }
+        public async Task<Response<string>> Handle(EditUserCommand request, CancellationToken cancellationToken)
+        {
+            var CheckUserEmail = await _userManager.FindByEmailAsync(request.Email);
+            if (CheckUserEmail != null)
+            {
+                return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.BadRequest]);
+            }
+            var CheckUserName = await _userManager.FindByNameAsync(request.UserName);
+            if (CheckUserName != null)
+            {
+                return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.BadRequest]);
+
+            }
+            var newUser = _mapper.Map<User>(request);
+            var createUser = await _userManager.CreateAsync(newUser);
+            if (!createUser.Succeeded)
+            {
+                return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.BadRequest]);
+
+            }
+
+            return SuccessMessage<string>(_stringLocalizer[SharedResourcesKeys.Success]);
         }
     }
 }
---
+++ SchoolProjectCore/Features/AppUser/Command/Model/EditUserCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SchoolProjectCore.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProjectCore.Features.AppUser.Command.Model
+{
+    public  class EditUserCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        public string? PhoneNumber { get; set; }
+    
+    }
+}
